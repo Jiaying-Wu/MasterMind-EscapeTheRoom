db321b876dc75e4a71ee7d5350b31cd79f05e409
Jiaying-Wu <raymond.wu@monash.edu>
2019-10-19 17:31:00 +1100

    create mastermind cpp and header file, add description text file

---
+++ project/masterMind.cpp
@@ -0,0 +1,13 @@
+/*********************************************
+Project: Master Mind - Escape the Room
+Author: Jiaying Wu
+Purpose: Driver Class File
+***********************************************/
+
+// header
+#include "masterMind.h"
+
+// main function
+int main(){
+
+}
---
+++ project/masterMind.h
@@ -0,0 +1,16 @@
+/**************************************************
+Project: Master Mind - Escape the Room
+Author: Jiaying Wu
+Purpose: Mastermind Class Header
+**************************************************/
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+// header
+
+
+// namespace
+
+
+#endif
